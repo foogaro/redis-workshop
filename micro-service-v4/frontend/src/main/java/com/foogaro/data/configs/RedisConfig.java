ae894bc97242c3c86694e46930b64b3e3a2213a7
Foogaro <luigi@foogaro.com>
2021-09-21 22:14:04 +0200

    Fixed connection properties for JedisPool adding the prefix spring. so it's going to be the same used by Spring Data Redis.

--- micro-service-v4/frontend/src/main/java/com/foogaro/data/configs/RedisConfig.java
+++ micro-service-v4/frontend/src/main/java/com/foogaro/data/configs/RedisConfig.java
@@ -41,10 +41,10 @@ public class RedisConfig {
 
     @Bean
     public JedisPool getJedisPool() {
-        String host = Conf4J.get("redis.host", "localhost");
-        String port = Conf4J.get("redis.port","6379");
-        String uid = Conf4J.get("redis.auth.uid","");
-        String pwd = Conf4J.get("redis.auth.pwd","");
+        String host = Conf4J.get("spring.redis.host", "localhost");
+        String port = Conf4J.get("spring.redis.port","6379");
+        String uid = Conf4J.get("spring.redis.auth.uid","");
+        String pwd = Conf4J.get("spring.redis.auth.pwd","");
         return (pwd.length() > 0) ? new JedisPool(host, Integer.valueOf(port), uid, pwd) : new JedisPool(host, Integer.valueOf(port));
     }
 
